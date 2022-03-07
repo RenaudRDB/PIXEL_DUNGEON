d0f1f5e14ad1b6d47a3d0517fc8c8325594ed97d
RenaudRDB <73218341+RenaudRDB@users.noreply.github.com>
2022-03-07 19:57:34 +0100

    fustible

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working app!</Text>
+      <Text>Non non non, vous n'avez pas dit le mot magique !</Text>
       <StatusBar style="auto" />
     </View>
   );
