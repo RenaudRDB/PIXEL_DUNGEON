577b71736503903bda6dd3f605dbe13908286c8a
RenaudRDB <73218341+RenaudRDB@users.noreply.github.com>
2022-03-13 11:54:10 +0100

    Echec

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Layout } from './src/components/Layout';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 
 export default function App() {
   return (
     <Layout>
         <Text>Non non non, vous n'avez pas dit le mot magique !</Text>
-        <NavigationContainer>
-        </NavigationContainer>
     </Layout>
   );
 }
