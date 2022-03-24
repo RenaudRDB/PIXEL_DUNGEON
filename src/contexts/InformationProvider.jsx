eaefab55cb240fdec423ee06852bce69554eb177
RenaudRDB <73218341+RenaudRDB@users.noreply.github.com>
2022-03-24 17:07:31 +0100

    Rajout des Provider

---
+++ src/contexts/InformationProvider.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useContext, createContext } from "react";
+
+const InformationContext = createContext();
+
+export const InformationProvider = ({children}) => {
+
+};
