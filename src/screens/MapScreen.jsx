320f2d29ac6f11ff4b792c248d4c736a968469ba
RenaudRDB <73218341+RenaudRDB@users.noreply.github.com>
2022-03-25 11:37:47 +0100

    testTableau

--- src/screens/MapScreen.jsx
+++ src/screens/MapScreen.jsx
@@ -1,10 +1,27 @@
 import { Text, View } from 'react-native';
 
+
+
 export const MapScreen = () => {
+
+  const test = [
+    ["Dragon", "Alice", "Renaud"],
+    ["EldenRing", "Violent", "Force"]
+  ]
+
   return (
     <View style={{ 
         flex: 1,
         backgroundColor:'black' }}>
+          {test.map((items, index) =>{
+            return (
+              <ol>
+                {items.map((subItems, sIndex)=> {
+                  return <li>{subItems}</li>;
+                })}
+              </ol>
+            );
+          })}
     </View>
   );
 }
