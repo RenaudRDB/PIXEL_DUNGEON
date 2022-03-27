524a5b382311c3eb9d5e4485fa33bb3054f38d82
Jérémy Barré <73218559+JeremyBarre@users.noreply.github.com>
2022-03-27 22:18:22 +0200

    Button Logout

--- src/navigators/TabNavigator.jsx
+++ src/navigators/TabNavigator.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ROUTES } from "./routes";
+import { Image } from "react-native";
 
 import { ActionScreen } from "../screens/ActionScreen";
 import { MapScreen } from "../screens/MapScreen";
@@ -11,7 +12,23 @@ const Tab = createBottomTabNavigator();
 
 export const TabNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+    // screenOptions={({ route }) => ({
+    //   tabBarIcon: ({ focused }) => {
+    //     let iconName;
+    //     if (route.name === ROUTES.ACTION) {
+    //       iconName = focused
+    //         ? require("../assets/.png")
+    //         : require("../assets/.png");
+    //     } else if (route.name === ROUTES.MAP) {
+    //       iconName = focused
+    //         ? require("../assets/.png")
+    //         : require("../assets/.png");
+    //     }
+    //     return <Image source={iconName} />;
+    //   },
+    // })}
+    >
       <Tab.Screen options={{ headerShown: false }} name={ROUTES.ACTION} component={ActionScreen} />
       <Tab.Screen options={{ headerShown: false }} name={ROUTES.MAP}component={MapScreen} />
       <Tab.Screen options={{ headerShown: false }} name={ROUTES.STAT} component={StatScreen} />
--- src/screens/ProfilScreen.jsx
+++ src/screens/ProfilScreen.jsx
@@ -1,11 +1,14 @@
-import { Text, View } from 'react-native';
+import { Text,Button, View } from 'react-native';
+import { useAuth } from '../contexts/AuthProvider';
 
 export const ProfilScreen = () => {
+  const { logout } = useAuth()
   return (
 	<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <Text>
       Profile
     </Text>
+    <Button title="Logout" onPress={() => logout()} />
 	</View>
   );
 }
